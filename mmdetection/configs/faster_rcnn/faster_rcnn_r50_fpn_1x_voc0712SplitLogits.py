2350a510d99eeee2be27c28fd338007dc97fd569
dimity <dimity.miller@csiro.au>
2021-07-27 13:50:49 +1000

    removing mmdet as a fork

--- mmdetection/configs/faster_rcnn/faster_rcnn_r50_fpn_1x_voc0712SplitLogits.py
+++
@@ -1,5 +0,0 @@
-_base_ = [
-    '../_base_/models/faster_rcnn_r50_fpn_vocSplitLogits.py',
-    '../_base_/datasets/voc0712Split.py',
-    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
-]
