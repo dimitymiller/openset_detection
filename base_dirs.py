f4148670a14204bf13f88009e1a767eb7a802aca
Dimity Miller <41029982+dimitymiller@users.noreply.github.com>
2021-11-05 10:39:14 +1000

    Update base_dirs.py

--- base_dirs.py
+++ base_dirs.py
@@ -1,6 +1,6 @@
 import os
 
 BASE_DIR_FOLDER = os.path.dirname(os.path.realpath(__file__))
-BASE_WEIGHTS_FOLDER = BASE_DIR_FOLDER+'/mmdetection/workDirs'
+BASE_WEIGHTS_FOLDER = BASE_DIR_FOLDER+'/mmdetection/weights'
 BASE_DATA_FOLDER = BASE_DIR_FOLDER+'/datasets/data'
 BASE_RESULTS_FOLDER = BASE_DIR_FOLDER + '/results'
