ca86feaee8c9b96535780392f92d2e403a90a70c
dimity <dimity.miller@csiro.au>
2022-05-04 13:46:10 +1000

    added minimum uncertainty error as a metric

--- mmdetection/performance_metrics.py
+++ mmdetection/performance_metrics.py
@@ -14,27 +14,44 @@ def tprAtFpr(tpr, fpr, fprRate = 0.05):
 
 	return tpratfpr, fpr[fprIdx]
 
+def minUE(inData, outData):
+	allData = np.concatenate((inData, outData))
+
+	thresholds = np.sort(allData)
+	uncertainty_error = []
+	#assuming that lower means more uncertain, less confident
+	for t in thresholds:
+		#number of in detections that are rejected incorrectly
+		in_error = np.sum(inData < t)/len(inData)
+		#number of out detections that are accepted incorrectly
+		out_error = np.sum(outData >= t)/len(outData)
+
+		ue = 0.5*in_error + 0.5*out_error
+		uncertainty_error += [ue]
+	
+	return np.min(uncertainty_error), uncertainty_error
+		
 def summarise_performance(inData, outData, fprRates = [], printRes = True, methodName = ''):
-    results = {}
+	results = {}
 
-    fpr, tpr, auroc = aurocScore(inData, outData)
-    results['auroc'] = auroc
-    results['fpr'] = list(fpr)
-    results['tpr'] = list(tpr)
+	fpr, tpr, auroc = aurocScore(inData, outData)
+	results['auroc'] = auroc
+	results['fpr'] = list(fpr)
+	results['tpr'] = list(tpr)
 
-    specPoints = []
-    for fprRate in fprRates:
-        tprRate = tprAtFpr(tpr, fpr, fprRate)
-        specPoints += [tprRate]
+	specPoints = []
+	for fprRate in fprRates:
+		tprRate = tprAtFpr(tpr, fpr, fprRate)
+		specPoints += [tprRate]
 
-        results[f'tpr at fprRate {fprRate}'] = tprRate
-    
-    if printRes:
-        print(f'Results for Method: {methodName}')
-        print(f'------ AUROC: {round(auroc, 3)}')
-        for point in specPoints:
-            fp = point[1]
-            tp = point[0]
-            print(f'------ TPR at {round((100.*fp), 1)}FPR: {round((100.*tp), 1)}')
+		results[f'tpr at fprRate {fprRate}'] = tprRate
+	
+	if printRes:
+		print(f'Results for Method: {methodName}')
+		print(f'------ AUROC: {round(auroc, 3)}')
+		for point in specPoints:
+			fp = point[1]
+			tp = point[0]
+			print(f'------ TPR at {round((100.*fp), 1)}FPR: {round((100.*tp), 1)}')
 
-    return results
+	return results
